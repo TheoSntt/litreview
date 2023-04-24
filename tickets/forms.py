93a0cdd69836006fc8f8c1f777e5ba3eec8d1a21
Théo Sinatti <sinatti.theo@gmail.com>
2023-04-24 16:27:07 +0200

    Added option to delete a ticket on same page as update. Also fixed update for it to be able to load images

--- tickets/forms.py
+++ tickets/forms.py
@@ -3,6 +3,10 @@ from django import forms
 from tickets.models import Ticket
 
 class TicketForm(forms.ModelForm):
+   edit_ticket = forms.BooleanField(widget=forms.HiddenInput, initial=True)
    class Meta:
      model = Ticket
      fields = ['title', 'description', 'image']
+
+class DeleteTicketForm(forms.Form):
+    delete_ticket = forms.BooleanField(widget=forms.HiddenInput, initial=True)
--- tickets/views.py
+++ tickets/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 from tickets.models import Ticket
-from tickets.forms import TicketForm
+from tickets.forms import TicketForm, DeleteTicketForm
 
 @login_required
 def create_ticket(request):
@@ -27,16 +27,27 @@ def create_ticket(request):
 @login_required
 def update_ticket(request, id):
     ticket = Ticket.objects.get(id=id)
+    edit_form = TicketForm(instance=ticket)
+    delete_form = DeleteTicketForm()
     if request.method == 'POST':
-        form = TicketForm(request.POST, instance=ticket)
-        if form.is_valid():
-            # mettre à jour le groupe existant dans la base de données
-            form.save()
-            # rediriger vers la page détaillée du groupe que nous venons de mettre à jour
-            return redirect('home')
-    else:
-        form = TicketForm(instance=ticket)
+        if 'edit_ticket' in request.POST:
+            edit_form = TicketForm(request.POST, request.FILES, instance=ticket)
+            if edit_form.is_valid():
+                # mettre à jour le groupe existant dans la base de données
+                edit_form.save()
+                # rediriger vers la page détaillée du groupe que nous venons de mettre à jour
+                return redirect('home')
+        if 'delete_ticket' in request.POST:
+            delete_form = DeleteTicketForm(request.POST)
+            if delete_form.is_valid():
+                # mettre à jour le groupe existant dans la base de données
+                ticket.delete()
+                # rediriger vers la page détaillée du groupe que nous venons de mettre à jour
+                return redirect('home')
+        
+    context = {
+        'edit_form': edit_form,
+        'delete_form': delete_form,
+        }     
 
-    return render(request,
-                'tickets/update_ticket.html',
-                {'form': form})
+    return render(request,'tickets/update_ticket.html', context=context)
