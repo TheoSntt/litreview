fe596f3ad4038114a86e3321725a1291f2fb4e2a
Théo Sinatti <sinatti.theo@gmail.com>
2023-04-25 16:54:39 +0200

    First modifications towards reviews in feed

--- feed/views.py
+++ feed/views.py
@@ -1,11 +1,20 @@
+from itertools import chain
+
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from tickets.models import Ticket
+from reviews.models import Review
 
 
 
 @login_required
 def feed(request):
     tickets = Ticket.objects.all()
+    reviews= Review.objects.all()
+    tickets_and_reviews = sorted(
+        chain(tickets, reviews),
+        key=lambda instance: instance.time_created,
+        reverse=True
+    )
     return render(request, 'feed/feed.html',
-    {'tickets': tickets})
+    {'tickets_and_reviews': tickets_and_reviews})
