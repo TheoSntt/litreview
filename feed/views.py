18bf843a5a2553baa6930031581e24f307e68e8d
Théo Sinatti <sinatti.theo@gmail.com>
2023-04-27 16:38:05 +0200

    Implement pagination.

--- feed/views.py
+++ feed/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from tickets.models import Ticket
 from reviews.models import Review
+from django.core.paginator import Paginator
 
 
 
@@ -19,8 +20,14 @@ def feed(request):
 
     tickets_and_reviews = request.user.get_feed()
     logged_user = request.user
+
+    paginator = Paginator(tickets_and_reviews, 2)
+    
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
     context={
-        'tickets_and_reviews':tickets_and_reviews,
+        'page_obj':page_obj,
         'logged_user':logged_user
     }
 
@@ -38,8 +45,13 @@ def users_posts_feed(request):
 
     tickets_and_reviews = request.user.get_own_posts_feed()
     logged_user = request.user
+
+    paginator = Paginator(tickets_and_reviews, 2)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
     context={
-        'tickets_and_reviews':tickets_and_reviews,
+        'page_obj':page_obj,
         'logged_user':logged_user
     }
 
