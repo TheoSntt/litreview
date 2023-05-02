9e622c4d5fbb4083a9ec61a8486bae8a87b77bc7
Théo Sinatti <sinatti.theo@gmail.com>
2023-05-02 17:03:28 +0200

    Make all authentication forms ultra crispy

--- litreview/settings.py
+++ litreview/settings.py
@@ -80,7 +80,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                # 'crispy_forms.context_processors.crispy_forms',
             ],
         },
     },
