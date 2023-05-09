06f4f56f4aae80df403fc5f41671518e06944430
enfants <enfants@imac-de-zoe-1.home>
2023-05-09 16:38:48 +0200

    Remove the change password button as it is not in the wireframes. Styled the feed page navigation buttons

--- litreview/settings.py
+++ litreview/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+# SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = 'django-insecure-uxx-lytt562k56!%(iph*!io+5fhwv&h8xt&93s0$xq)sj4ym-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
