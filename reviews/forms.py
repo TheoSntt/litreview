ef27cca38d9dc26572d7168fd3d4f274ffba02b7
Théo Sinatti <sinatti.theo@gmail.com>
2023-05-10 14:22:15 +0200

    Minor tweaks to comply to the wireframes. Add text area for long text input. Add labels to fields in forms.

--- reviews/forms.py
+++ reviews/forms.py
@@ -10,8 +10,15 @@ class ReviewForm(forms.ModelForm):
     fields = ['headline', 'rating', 'body']
     widgets = {
       'rating': forms.RadioSelect(choices=[(0, '0'), (1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5')],
-                                  attrs={"class":"tropclassecetteclass"})
-                                  }
+                                  attrs={"class":"tropclassecetteclass"}),
+      'body': forms.Textarea(attrs={'rows': 6}),
+      }
+    labels={
+      'headline': 'Titre',
+      'rating': 'Note',
+      'body': 'Commentaire',
+
+        }
 
 class DeleteReviewForm(forms.Form):
   delete_review = forms.BooleanField(widget=forms.HiddenInput, initial=True)
--- tickets/forms.py
+++ tickets/forms.py
@@ -5,10 +5,16 @@ from crispy_forms.layout import Submit
 from tickets.models import Ticket
 
 class TicketForm(forms.ModelForm):
-   edit_ticket = forms.BooleanField(widget=forms.HiddenInput, initial=True)
-   class Meta:
-     model = Ticket
-     fields = ['title', 'description', 'image']
+  edit_ticket = forms.BooleanField(widget=forms.HiddenInput, initial=True)
+  class Meta:
+    model = Ticket
+    fields = ['title', 'description', 'image']
+    widgets = {
+       "description": forms.Textarea(attrs={'rows': 6}),
+    }
+    labels={
+      'title': 'Titre',
+    }
 
 class DeleteTicketForm(forms.Form):
     delete_ticket = forms.BooleanField(widget=forms.HiddenInput, initial=True)
